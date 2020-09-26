b3f770cb8fac83780aaf50ba2f780bd13a6e8c07
yanivsultan <yanivsultan5@gmail.com>
2020-09-26 21:31:39 +0300

    working on search page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Home from './Home';
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
 
-      <Header />
-
-      <Home />
-      
-
-      {/* Banner */}
-
-      {/* cards */}
-
-      {/* Footer */}
-
-  {/* Search Page and more... */}
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>©2020 Airbnb Clone, No rights reserved - This is a demo.</p>
+            <p>Privacy · Terms · Sitemap · Company deatils</p>
+        </div>
+    )
+}
+
+export default Footer
